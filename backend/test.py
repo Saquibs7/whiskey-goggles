0a322c09d3f8f27d0c60c4e301841c134ead1005
saquibs7 <saquibhussain1289@gmail.com>
2025-05-01 22:47:44 +0530

    requirements.txt updated

--- backend/test.py
+++ backend/test.py
@@ -1,7 +1,7 @@
 import requests
 
 url = "http://127.0.0.1:5000/api/search"
-files = {'image': open('sample2.jpg', 'rb')}
+files = {'image': open('backend/sample2.jpg', 'rb')}
 response = requests.post(url, files=files)
 
 print(response.status_code)
